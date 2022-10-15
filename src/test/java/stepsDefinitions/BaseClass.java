0273307daaa2b88a7ad615a507573a079cca948d
alanliew8888 <keanoon03@gmail.com>
2022-10-15 14:37:58 +0800

    Initialize the new searchcustomerpage as sp

--- src/test/java/stepsDefinitions/BaseClass.java
+++ src/test/java/stepsDefinitions/BaseClass.java
@@ -4,12 +4,15 @@ import org.apache.commons.lang.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
 import pageObjects.AddCustomerPage;
 import pageObjects.LoginPage;
+import pageObjects.SearchCustomerPage;
 
 public class BaseClass {
     public WebDriver driver;
     public LoginPage lp;
 
     public AddCustomerPage ap;
+
+    public SearchCustomerPage sp;
 
     public static String randomstring(){
         String generatedString1 = RandomStringUtils.randomAlphabetic(5);
